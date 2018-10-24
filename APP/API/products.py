8ce697757928559eb0ead22f8010acb11be4af91
masete <nicholasmasete72@gmail.com>
2018-10-24 11:06:29 +0300

    [Delivers #161438251]
    Add admin endpoint to add products

--- APP/API/products.py
+++ APP/API/products.py
@@ -7,6 +7,17 @@ def start_app():
 
 pdtsLists = []
 
+@app.route("/api/v1/products", methods=["POST"],strict_slashes=False)
+def post_a_product():
+    mum = request.get_json()
+    product_id = len(pdtsLists) + 1
+    product_name = mum['product_name']
+    product_price = mum['product_price']
+
+    pdtsLists.append(AllProducts(product_id, product_name, product_price))
+    return jsonify({"Products":"a product now exists"}), 201
+
+
 @app.route("/api/v1/products/", methods=["GET"],strict_slashes=False)
 def get_all_prducts():
     getback = []
