b71fce6c71aa2432b3ec8a791301379693d32839
masete <nicholasmasete72@gmail.com>
2018-10-27 14:06:03 +0300

    Add changes in tests

--- tests/product_tests.py
+++ tests/product_tests.py
@@ -16,7 +16,7 @@ class TestEndpoints(unittest.TestCase):
             self.assertEqual(feedback.status_code, 400)
             print(feedback.data)
             feedbackJson = json.loads(feedback.data.decode())
-            self.assertIn('Product field can not have space', feedbackJson['Products'])
+            self.assertIn('Product field can not have space', feedbackJson['message'])
             self.assertTrue(feedback.content_type, 'application/json')
 
     def test_post_a_product_is_passed_empty_field(self):
@@ -27,7 +27,7 @@ class TestEndpoints(unittest.TestCase):
                                                          )))
             self.assertEqual(feedback.status_code, 400)
             feedbackJson = json.loads(feedback.data.decode())                                                     
-            self.assertIn('Product field can not be empty', feedbackJson['Products'])
+            self.assertIn('Product field can not be empty', feedbackJson['message'])
 
     def test_post_a_product_price_is_passed_empty_field(self):
         with self.app.test_client() as client:
@@ -37,7 +37,7 @@ class TestEndpoints(unittest.TestCase):
                                                          )))
             self.assertEqual(feedback.status_code, 400)
             feedbackJson = json.loads(feedback.data.decode())                                                     
-            self.assertIn('Product price field cant be empty', feedbackJson['Products']) 
+            self.assertIn('Product price field cant be empty', feedbackJson['message']) 
 
     def test_post_a_product_price_is_passed_empty_string(self):
         with self.app.test_client() as client:
@@ -47,7 +47,7 @@ class TestEndpoints(unittest.TestCase):
                                                          )))
             self.assertEqual(feedback.status_code, 400)
             feedbackJson = json.loads(feedback.data.decode())                                                     
-            self.assertIn('Product price field cant be passed empty string', feedbackJson['Products'])            
+            self.assertIn('Product price field cant be passed empty string', feedbackJson['message'])            
 
     def test_post_a_product_price_is_int(self):
         with self.app.test_client() as client:
@@ -57,7 +57,7 @@ class TestEndpoints(unittest.TestCase):
                                                          )))
             self.assertEqual(feedback.status_code, 400)
             feedbackJson = json.loads(feedback.data.decode())                                                     
-            self.assertIn('Product price field can not be a string', feedbackJson['Products'])                                                    
+            self.assertIn('Product price field can not be a string', feedbackJson['message'])                                                    
     
     def test_post_a_product_name_is_string(self):
         with self.app.test_client() as client:
@@ -67,7 +67,7 @@ class TestEndpoints(unittest.TestCase):
                                                          )))
             self.assertEqual(feedback.status_code, 400)
             feedbackJson = json.loads(feedback.data.decode())                                                     
-            self.assertIn('Product name field can not be an int', feedbackJson['Products'])  
+            self.assertIn('Product name field can not be an int', feedbackJson['message'])  
 
     def test_product(self):
         with self.app.test_client() as client:
@@ -76,7 +76,7 @@ class TestEndpoints(unittest.TestCase):
                                                         product_price=1500000)))
             self.assertEqual(feedback.status_code, 201)
             feedbackJson = json.loads(feedback.data.decode())
-            self.assertIn("a product now exists", feedbackJson["Products"])
+            self.assertIn("a product now exists", feedbackJson["message"])
             self.assertTrue(feedback.content_type, 'application/json')
 
     def test_get_products(self):
@@ -99,7 +99,7 @@ class TestEndpoints(unittest.TestCase):
                                                         )))
             self.assertEqual(feedback.status_code, 400)
             feedbackJson = json.loads(feedback.data.decode())
-            self.assertIn('Sale field can not have space', feedbackJson['Sales'])
+            self.assertIn('Sale field can not have space', feedbackJson['message'])
             self.assertTrue(feedback.content_type, 'application/json')        
 
 
@@ -112,7 +112,7 @@ class TestEndpoints(unittest.TestCase):
                                                         )))
             self.assertEqual(feedback.status_code, 400)
             feedbackJson = json.loads(feedback.data.decode())
-            self.assertIn('Sale field can not be passed empty string', feedbackJson['Sales'])
+            self.assertIn('Sale field can not be passed empty string', feedbackJson['message'])
             self.assertTrue(feedback.content_type, 'application/json')    
                           
     def test_post_a_sale_price_is_int(self):
@@ -123,7 +123,7 @@ class TestEndpoints(unittest.TestCase):
                                                          )))
             self.assertEqual(feedback.status_code, 400)
             feedbackJson = json.loads(feedback.data.decode())                                                     
-            self.assertIn('sale price field can not be a string', feedbackJson['Sales']) 
+            self.assertIn('sale price field can not be a string', feedbackJson['message']) 
 
     def test_post_a_sale_price_is_passed_empty_field(self):
         with self.app.test_client() as client:
@@ -133,7 +133,7 @@ class TestEndpoints(unittest.TestCase):
                                                          )))
             self.assertEqual(feedback.status_code, 400)
             feedbackJson = json.loads(feedback.data.decode())                                                     
-            self.assertIn('Sales price field cant be empty', feedbackJson['Sales'])  
+            self.assertIn('Sales price field cant be empty', feedbackJson['message'])  
 
     def test_post_a_sale_price_is_passed_empty_string(self):
         with self.app.test_client() as client:
@@ -143,7 +143,7 @@ class TestEndpoints(unittest.TestCase):
                                                          )))
             self.assertEqual(feedback.status_code, 400)
             feedbackJson = json.loads(feedback.data.decode())                                                     
-            self.assertIn('Sales price field cant be passed an empty string', feedbackJson['Sales']) 
+            self.assertIn('Sales price field cant be passed an empty string', feedbackJson['message']) 
 
     def test_post_a_sale_quantity_is_string(self):
         with self.app.test_client() as client:
@@ -153,7 +153,7 @@ class TestEndpoints(unittest.TestCase):
                                                          )))
             self.assertEqual(feedback.status_code, 400)
             feedbackJson = json.loads(feedback.data.decode())                                                     
-            self.assertIn('sale quantity field can not be an int', feedbackJson['Sales'])
+            self.assertIn('sale quantity field can not be an int', feedbackJson['message'])
 
     def test_get_one_sale(self):
         with self.app.test_client() as client:
